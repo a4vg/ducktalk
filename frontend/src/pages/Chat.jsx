c2765d2fe5ff65cb1a4a0d17782c9b7460e83581
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2021-08-06 15:07:19 -0400

    Add create new chat in frontend

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -7,6 +7,7 @@ import API from "../api/api";
 
 import Avatar from "../components/Avatar";
 import MessagesArea from "../components/MessagesArea";
+import NewChatForm from "../components/NewChatForm";
 
 const Chat = () => {
   const indexDB = useIndexedDB("keys");
@@ -166,6 +167,7 @@ const Chat = () => {
             Chats
           </h2>
           <div className="overflow-y-auto">
+            <NewChatForm />
             {chats.length !== 0 ? (
               chats.map(chat => (
                 <div
@@ -193,7 +195,7 @@ const Chat = () => {
         </div>
       </div>
       {/* Right panel */}
-      {chats.length !== 0 && (
+      {chats.length !== 0 ? (
         <div
           className={`md:w-9/12 sm:w-7/12 h-full transition-all duration-500 w-full p-2 flex flex-col space-y-2`}
         >
@@ -241,6 +243,16 @@ const Chat = () => {
             </button>
           </div>
         </div>
+      ) : (
+        <svg
+          className={`md:hidden h-10 w-10 fill-current text-orange-500 cursor-pointer ml-5 mt-4  ${
+            showChats ? "" : "block"
+          }`}
+          viewBox="0 0 20 20"
+          onClick={() => setShowChats(!showChats)}
+        >
+          <path d="M11.739,13.962c-0.087,0.086-0.199,0.131-0.312,0.131c-0.112,0-0.226-0.045-0.312-0.131l-3.738-3.736c-0.173-0.173-0.173-0.454,0-0.626l3.559-3.562c0.173-0.175,0.454-0.173,0.626,0c0.173,0.172,0.173,0.451,0,0.624l-3.248,3.25l3.425,3.426C11.911,13.511,11.911,13.789,11.739,13.962 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.148,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.148,17.521,10"></path>{" "}
+        </svg>
       )}
     </div>
   ) : (
